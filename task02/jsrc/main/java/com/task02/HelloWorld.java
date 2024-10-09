a139cf04416e7eb9c5b1375b315abe8c91021e85
Muhammadsolih <muhammadsolihumarov@gmail.com>
2024-10-09 18:06:50 +0500

    done

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -2,26 +2,36 @@ package com.task02;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-import com.syndicate.deployment.model.RetentionSetting;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@LambdaHandler(
-    lambdaName = "hello_world",
-	roleName = "hello_world-role",
-	isPublishVersion = true,
-	aliasName = "${lambdas_alias_name}",
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
-)
-public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
+public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
-		return resultMap;
+	@Override
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
+		String path = request.getPath();
+		String method = request.getHttpMethod();
+
+		APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Content-Type", "application/json");
+		response.setHeaders(headers);
+
+		// Handle /hello path with GET method
+		if ("/hello".equals(path) && "GET".equalsIgnoreCase(method)) {
+			response.setStatusCode(200);
+			response.setBody("{\"message\": \"Hello from Lambda\"}");
+		}
+		// Handle other paths with 400 Bad Request
+		else {
+			response.setStatusCode(400);
+			response.setBody("{\"message\": \"Bad request syntax or unsupported method. " +
+					"Request path: " + path + ". HTTP method: " + method + "\"}");
+		}
+
+		return response;
 	}
 }
